387ae6bd82689c63cc5fb449583a2958b9d02074
Hyun <itsmine096@gmail.com>
2023-01-18 19:51:27 +0900

    [add] 스프링 부트 프로젝트 생성

---
+++ src/test/java/hyun/concurrencycontrolprac/ConcurrencyControlPracApplicationTests.java
@@ -0,0 +1,13 @@
+package hyun.concurrencycontrolprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyControlPracApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
